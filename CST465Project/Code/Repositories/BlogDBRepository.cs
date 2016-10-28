7f9e41e29c114f8be390a390d6abe2f1fa8b4701
Samuel Huff <contact@samueljhuff.com>
2016-10-27 20:47:31 -0700

    Lab 5: Repositoried about my blogs

---
+++ CST465Project/Code/Repositories/BlogDBRepository.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CST465Project
+{
+    public class BlogDBRepository : IDataEntityRepository<BlogPost>
+    {
+        public BlogPost Get(int id)
+        {
+            BlogPost outValue = new BlogPost();
+
+            using (SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Aura"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM Blog WHERE ID=@ID";
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            outValue.ID = id;
+                            outValue.Author = reader["Author"].ToString();
+                            outValue.Title = reader["Title"].ToString(); ;
+                            outValue.Content = reader["Content"].ToString();
+                            outValue.Timestamp = DateTime.Parse(reader["Timestamp"].ToString());
+                        }
+                    }
+                } 
+            }
+
+            return outValue;
+        }
+
+        public List<BlogPost> GetList()
+        {
+            List<BlogPost> outValue = new List<BlogPost>();
+
+            using (SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Aura"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM Blog";
+                    cmd.CommandType = CommandType.Text;
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        BlogPost temp = new BlogPost();
+                        temp.ID = (int)reader["ID"];
+                        temp.Author = reader["Author"].ToString();
+                        temp.Title = reader["Title"].ToString();
+                        temp.Content = reader["Content"].ToString();
+                        temp.Timestamp = DateTime.Parse(reader["Timestamp"].ToString());
+                        outValue.Add(temp);
+                    }
+                }
+            }
+
+            return outValue;
+        }
+
+        public void Save(BlogPost entity)
+        {
+            using (SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Aura"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+
+                if (entity.ID != 0)
+                {
+                    cmd.CommandText = "UPDATE Blog SET Author=@Author, Title=@Title, Content=@Content WHERE ID=@ID";
+                    cmd.Parameters.AddWithValue("@ID", entity.ID);
+                }
+                else
+                    cmd.CommandText = "INSERT INTO Blog (Author, Title, Content) VALUES (@Author, @Title, @Content)";
+
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Author", entity.Author);
+
+                cmd.Parameters.AddWithValue("@Title", entity.Title);
+                cmd.Parameters.AddWithValue("@Content", entity.Content);
+                cmd.Connection.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ CST465Project/Controllers/BlogController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CST465Project.Controllers
+{
+    public class BlogController : Controller
+    {
+        private IDataEntityRepository<BlogPost> _repository;
+        public BlogController()
+        {
+            this._repository = new BlogDBRepository();
+        }
+        // GET: Blog
+        public ActionResult Index()
+        {
+            List<BlogPost> list = _repository.GetList();
+            return View(list);
+        }
+
+        public ActionResult Add()
+        {
+            BlogPostModel model = new BlogPostModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Add(BlogPostModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                BlogPost post = new BlogPost();
+                post.ID = model.ID;
+                post.Title = model.Title;
+                post.Content = model.Content;
+                post.Author = model.Author;
+                _repository.Save(post);
+            }
+            else
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            BlogPost post = _repository.Get(id);
+            BlogPostModel postmodel = new BlogPostModel();
+            postmodel.Author = post.Author;
+            postmodel.Content = post.Content;
+            postmodel.ID = post.ID;
+            postmodel.Title = post.Title;
+
+            return View(postmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(BlogPostModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                BlogPost post = new BlogPost();
+                post.Author = model.Author;
+                post.Content = model.Content;
+                post.ID = model.ID;
+                post.Title = model.Title;
+                _repository.Save(post);
+            }
+            else
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
