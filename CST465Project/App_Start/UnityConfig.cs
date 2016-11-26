60ca9ffdee939842b31f05e56bafdc5a99dffbf1
Sam Huff <Sam Huff>
2016-11-26 11:22:35 -0800

    Login page redirect working

--- CST465Project/App_Start/UnityConfig.cs
+++ CST465Project/App_Start/UnityConfig.cs
@@ -1,6 +1,10 @@
 using System.Web.Mvc;
 using Microsoft.Practices.Unity;
 using Unity.Mvc5;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using CST465Project.Controllers;
 
 namespace CST465Project
 {
@@ -14,6 +18,11 @@ namespace CST465Project
             // it is NOT necessary to register your controllers
 
             // e.g. container.RegisterType<ITestService, TestService>();
+            container.RegisterType<DbContext, ApplicationDbContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<UserManager<ApplicationUser>>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(new HierarchicalLifetimeManager());
+            container.RegisterType<IRoleStore<ApplicationRole, string>, RoleStore<ApplicationRole>>(new HierarchicalLifetimeManager());
+            container.RegisterType<AccountController>(new InjectionConstructor());
             container.RegisterType<IDataEntityRepository<BlogPost>, BlogJSONRepository>();
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
---
+++ CST465Project/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CST465Project
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int ProfileID { get; set; }
+        //private DateTime _BirthDate = DateTime.Now;
+        public DateTime BirthDate { get; set; }//{ get { return _BirthDate; } set{ _BirthDate = value; } }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
